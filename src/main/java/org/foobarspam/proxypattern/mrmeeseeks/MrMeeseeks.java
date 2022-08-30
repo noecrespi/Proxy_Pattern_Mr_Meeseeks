00b7ea2ee33014be9c62d3c30c3972b18637aa98
Noelia <ncrespipomar@cifpfbmoll.eu>
2022-08-31 01:22:54 +0200

    create one part of request

--- src/main/java/org/foobarspam/proxypattern/mrmeeseeks/MrMeeseeks.java
+++ src/main/java/org/foobarspam/proxypattern/mrmeeseeks/MrMeeseeks.java
@@ -1,6 +1,7 @@
 package org.foobarspam.proxypattern.mrmeeseeks;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -9,7 +10,7 @@ public class MrMeeseeks {
 
 
     //ATRIBUTO CONSTANTE
-// inicializamos y decimoa que empiece a partir del número mil
+    // inicializamos y decimoa que empiece a partir del número mil
     private static AtomicInteger ID_GENERATOR = new AtomicInteger(1000);
 
     // ATRIBUTOS
@@ -18,9 +19,9 @@ public class MrMeeseeks {
     private String messageOnDone;
     private String messageOnExplore;
     // lista llamda request que contiene string[]
-    //string[] -> otra forma de llamar a las listas
+    //string[] -> otra forma de llamar a las listas, longitud fija
     private String[] request;
-    //lista llamanda requetAsList que contiene Strings
+    //list llamanda requetAsList que contiene Strings, longitud variables, metodos definidos
     private List<String> requetAsList;
 
     //contructor
@@ -30,32 +31,76 @@ public class MrMeeseeks {
     }
 
     //Método
+
+    //Crear el id
     public Integer getId() {
         return Id;
     }
 
+    // muestra el 1º mensaje que sale en consola
+    // I'm Mr Meeseks 1000. Look at meeee!
     private void sayMessageOnCreate(){
         System.out.println("I'm Mr Meeseeks " + getId() + ". Look at meeee!") ;
     }
 
+    // Hay que sobreescribir el método para poder usarlo de tipo string
     @Override
     public String toString() {
-        // tenemos que pasar un String el id pero el id es un número numerioco tenemos que ...
+        // tenemos que pasar un String y como el id es de tipo numerico tenemos que convertirlo de tipo String
         return getId().toString();
     }
 
+    // obtener el mensaje de la request
     private String getMessageOnRequest(int numMessageOnRequest){
         return messageOnRequest;
     }
 
 
+    private void setRequest(String request){
+        //split lista...
+        this.request = request.split(" ");
+        this.requetAsList = Arrays.asList(this.request);
+    }
     
-    public void formulateRequest(String request){}
+    public void formulateRequest(String request){
+        // 1r llama al set request
+        setRequest(request);
+        fulfillRequest();
+    }
 
-    public void sayMessageOnExplode() {
+    // llama a las funciones de la request (son las tres últimas frases)
+    public void fulfillRequest(){
+        //llama para a los Request para que se muestren en pantalla
+        sayMessageOnRequest();
+        doRequest(getRequest());
+        sayMessageOnDone();
+    }
+
+    //Supuestamente deberia pasrse dos parametros pero en la app solo se pasa uno por lo que dejo en uno
+    public void doRequest(String request) {
+        System.out.println(request);
     }
 
     public String getRequest() {
-        return null;
+        return String.join(" ", this.request);
+    }
+
+    // pof que se ejecuta cuando se borra
+    public void sayMessageOnExplode() {
+        System.out.println("Pooooof " + getId());
+    }
+
+    // Mensaje que tiene que devolver All done!
+    private void sayMessageOnDone(){
+        System.out.println("All done!");
+    }
+
+    // all done private se ejecuta "say message on done"
+    // oh ye se ejecuta en say "message on request" ejecita un metodo llamado getmessage on request y te devuelve un
+    // mensaje random entre dos
+
+    // este método tiene que seleccionar una frase random de la request
+    public void sayMessageOnRequest(){
+
     }
 }
